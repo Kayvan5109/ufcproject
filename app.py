e74288da053ad79eb4c3c7da71e33f8d2d7ab5cf
Kayvan-Khoobehi <khoobehi.kayvan@gmail.com>
2023-04-24 17:57:04 -0500

    update

--- app.py
+++ app.py
@@ -1,80 +1,65 @@
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import random
+fighters_altered = pd.read_csv("fighters_altered.csv")
+pure_stat = pd.read_csv("Pure_Stat.csv")
+averages = pd.read_csv("averages.csv")
+def main():
+    st.sidebar.title("Navigation")
+    tab = st.sidebar.radio("Go to:", ["Home", "Single Fighter", "Compare Fighters", "Random Fighter", "Weight Class Averages"])
 
-def display_fight_history(fighter_name):
-    fight_history = ufc_stat[(ufc_stat["Fighter 1"].str.contains(fighter_name, case=False)) | (ufc_stat["Fighter 2"].str.contains(fighter_name, case=False))]
-    st.subheader(f"{fighter_name}'s Fight History")
-    st.write(fight_history)
+    if tab == "Home":
+        st.title("Welcome to the MMA Statistics App!")
+        st.write("Please use the sidebar to navigate between different sections of the app.")
+    elif tab == "Single Fighter":
+        single_fighter()
+    elif tab == "Compare Fighters":
+        compare_fighters()
+    elif tab == "Random Fighter":
+        random_fighter()
+    elif tab == "Weight Class Averages":
+        weight_class_averages()
 
-# Load the datasets
-ufc_stat = pd.read_csv("ufc_stat.csv")
-fighters = pd.read_csv("fighters.csv")
-ufc_avg = pd.read_csv("averages.csv")
-ufc_std = pd.read_csv("ufc_std.csv")
+if __name__ == "__main__":
+    main()
+def single_fighter():
+    st.title("Single Fighter")
+    name = st.text_input("Enter fighter's name:")
+    if name:
+        fighter_data = fighters_altered[fighters_altered["Name"] == name]
+        fight_data = pure_stat[pure_stat["Name"] == name]
+        if not fighter_data.empty:
+            st.write(fighter_data)
+            st.write(fight_data)
+        else:
+            st.warning("No data found for the entered fighter's name.")
 
-# Set page title
-st.set_page_config(page_title="UFC Fighter Stats")
+def compare_fighters():
+    st.title("Compare Fighters")
+    name1 = st.text_input("Enter first fighter's name:")
+    name2 = st.text_input("Enter second fighter's name:")
 
-# Set app header
-st.title("UFC Fighter Stats")
+    if name1 and name2:
+        fighter1_data = fighters_altered[fighters_altered["Name"] == name1]
+        fighter2_data = fighters_altered[fighters_altered["Name"] == name2]
 
-# Create tabs
-tab = st.sidebar.selectbox("Select a tab", ["Fighters", "Matchup Lookup"])
+        if not fighter1_data.empty and not fighter2_data.empty:
+            st.write("Fighter 1:")
+            st.write(fighter1_data)
+            st.write("Fighter 2:")
+            st.write(fighter2_data)
+        else:
+            st.warning("No data found for one or both of the entered fighter's names.")
 
-if tab == "Fighters":
-    # User input for fighter name
-    fighter_name = st.text_input("Enter a fighter's name:")
+def random_fighter():
+    st.title("Random Fighter")
+    if st.button("Generate Random Fighter"):
+        random_name = random.choice(fighters_altered["Name"].values)
+        fighter_data = fighters_altered[fighters_altered["Name"] == random_name]
+        fight_data = pure_stat[pure_stat["Name"] == random_name]
+        st.write(fighter_data)
+        st.write(fight_data)
 
-    # Filter fighter stats by name
-    fighter_stats = fighters[fighters["Fighter Name"].str.contains(fighter_name, case=False)]
-
-    # Display fighter stats and weight class stats
-    if not fighter_stats.empty:
-        st.subheader(fighter_name)
-        st.write(fighter_stats)
-
-        weight_class = fighter_stats.iloc[0]["Weight Class"]
-        weight_class_avg = ufc_avg[ufc_avg["Weight Class"] == weight_class].reset_index(drop=True)
-        weight_class_std = ufc_std[ufc_std["Weight Class"] == weight_class].reset_index(drop=True)
-
-        st.subheader(f"{weight_class} Average Stats")
-        st.write(weight_class_avg)
-
-        st.subheader(f"{weight_class} Standard Deviations")
-        st.write(weight_class_std)
-
-        display_fight_history(fighter_name)
-
-    else:
-        st.write("No fighter found with that name.")
-
-elif tab == "Matchup Lookup":
-    # User input for two fighter names
-    fighter1_name = st.text_input("Enter the first fighter's name:")
-    fighter2_name = st.text_input("Enter the second fighter's name:")
-
-    # Filter fighter stats by name
-    fighter1_stats = fighters[fighters["Fighter Name"].str.contains(fighter1_name, case=False)]
-    fighter2_stats = fighters[fighters["Fighter Name"].str.contains(fighter2_name, case=False)]
-
-    # Display fighter stats and weight class stats for both fighters
-    if not fighter1_stats.empty and not fighter2_stats.empty:
-        st.subheader(fighter1_name)
-        st.write(fighter1_stats)
-        st.subheader(fighter2_name)
-        st.write(fighter2_stats)
-        
-        weight_class1 = fighter1_stats.iloc[0]["Weight Class"]
-        weight_class1_avg = ufc_avg[ufc_avg["Weight Class"] == weight_class1].reset_index(drop=True)
-        weight_class1_std = ufc_std[ufc_std["Weight Class"] == weight_class1].reset_index(drop=True)
-
-        st.subheader(f"{weight_class1} Average Stats")
-        st.write(weight_class1_avg)
-
-        st.subheader(f"{weight_class1} Standard Deviations")
-        st.write(weight_class1_std)
-        
-        display_fight_history(fighter1_name)
-        display_fight_history(fighter2_name)
-    else:
-        st.write("Please enter valid fighter.")
+def weight_class_averages():
+    st.title("Weight Class Averages")
+    st.write(averages)
